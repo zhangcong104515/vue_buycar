3a34571a1b18ce0e9691c957b3f0b543bd15a541
zhangcong104515 <55452443+zhangcong104515@users.noreply.github.com>
2019-09-18 14:50:16 +0800

    Update shouye.vue
    添加客服和店铺选项

--- src/components/shouye.vue
+++ src/components/shouye.vue
@@ -14,8 +14,8 @@
   <el-submenu index="2">
     <template slot="title">我的工作台</template>
     <el-menu-item index="/shouye/buycar">购物车</el-menu-item>
-    <el-menu-item index="">haha</el-menu-item>
-    <el-menu-item index="2-3">选项3</el-menu-item>
+    <el-menu-item index="">客服</el-menu-item>
+    <el-menu-item index="2-3">店铺</el-menu-item>
     <el-submenu index="2-4">
       <template slot="title">选项4</template>
       <el-menu-item index="2-4-1">选项1</el-menu-item>
